0b08948a6a29b87e51b2ed8963be5960c9b3d688
Hadeed Ahmad <hadeedji@gmail.com>
2024-07-03 00:15:21 +0500

    Add some css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,19 @@
 export default {
   content: ["index.html"],
-  theme: {},
+  theme: {
+    colors: {
+      "brandy-red": "#854632",
+      "dark-charcoal": "#312E2C",
+      "dark-raspberry": "#7A284E",
+      "wenge-brown": "#5F564D",
+      "white-coffee": "#E3DDD7",
+      eggshell: "#F3E5D7",
+      snow: "#FFF7FB",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+      serif: ["Young\\ Serif", "serif"],
+    },
+  },
 };
